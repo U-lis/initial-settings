006a8d85d03e39f4a01f6cbf89fd31c124709760
U-lis <ds2ilz@hotmail.com>
2014-10-21 09:02:32 +0900

    install plugins in command line

--- vim/vimrc.sh
+++
@@ -1,7 +0,0 @@
-echo "vimrc setting requires Vundle"
-echo "Install Vundle..."
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
-echo "Vundle Installed. Copying .vimrc file..."
-cp .vimrc ~/
-echo "You have to start vim and use ':PluginInstall' command to install vundles"
-echo "You may have to install font for powerline"
