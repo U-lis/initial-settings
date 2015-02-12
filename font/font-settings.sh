3aa97229d65513dc70ea2fb8ef3bd917b3dcc97b
U-lis <ds2ilz@hotmail.com>
2015-02-12 13:59:32 +0900

    시스템폰트 사용 옵션 비활성

--- font/font-settings.sh
+++ font/font-settings.sh
@@ -12,8 +12,9 @@ echo "DejaVu Sans Mono for powerline Installed"
 if [ -z $2 ]; then
     echo "Please set your terminal's default font to one of them"
     echo "to use your shell in good design"
-else
+elif [ $2 != 1 ]; then
     echo "You choose theme $2 so we will set font to Inconsolata-dz for Powerline"
-    gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Inconsolata-dz for Poiwerline Medium 11"
+    gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type bool false
+    gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Inconsolata-dz for Powerline Medium 11"
     echo "Terminal font change done"
 fi
