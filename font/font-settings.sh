aac835a4bea7db5e1c39c06381e26580925fedd8
U-lis <ulismoon@gmail.com>
2016-07-23 03:02:06 +0900

    use dconf for gnome 3.8+ #1

--- font/font-settings.sh
+++ font/font-settings.sh
@@ -14,7 +14,17 @@ if [ -z $2 ]; then
     echo "to use your shell in good design"
 elif [ $2 != 1 ]; then
     echo "You choose theme $2 so we will set font to Inconsolata-dz for Powerline"
-    gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type bool false
-    gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Inconsolata-dz for Powerline Medium 11"
+    shell_version="$(gnome-shell --version)"
+    IFS=' '; arr=($shell_version)
+    IFS='.'; ver=(${arr[2]})
+    minor_version=${ver[1]}
+    if [ "$minor_version" -lt "8" ]; then
+        gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type bool false
+        gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Inconsolata-dz for Powerline Medium 12"
+    else
+        default_profile="b1dcc9dd-5262-4d8d-a863-c897e6d979b9"
+        dconf write /org/gnome/terminal/legacy/profiles:/:$default_profile/use-system-font "false"
+        dconf write /org/gnome/terminal/legacy/profiles:/:$default_profile/font "'Inconsolata-dz for Powerline Medium 12'"
+    fi
     echo "Terminal font change done"
 fi
